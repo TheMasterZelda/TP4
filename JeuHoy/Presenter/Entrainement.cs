ffb437a68b76ff0daa46174802b6d57187df9dfc
TheMasterZelda <davidfoulernier@hotmail.ca>
2017-04-29 14:17:35 -0400

    Presenter

---
+++ JeuHoy/Presenter/Entrainement.cs
@@ -0,0 +1,72 @@
+﻿using JeuHoy.Vue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace JeuHoy.Presenter
+{
+    public class Entrainement
+    {
+        private IVue _vue;
+
+        public Entrainement(IVue vue)
+        {
+            _vue = vue;
+            _vue.ChangerFigure += btnClickChangerFigure_Click;
+            _vue.Apprendre += btnApprendre_Click;
+            _vue.iPosition = 1;
+            _vue.DicImgFigure = new Dictionary<string, Image>();
+        }
+
+
+
+        /// <summary>
+        /// Charger la figure de danse en cours.
+        /// </summary>
+        private void ChargerFigure()
+        {
+            Image imgValue;
+            bool bResultat;
+
+            if (_vue.iPosition > CstApplication.NBFIGURE)
+                _vue.iPosition = 1;
+
+            if (_vue.iPosition < 1)
+                _vue.iPosition = CstApplication.NBFIGURE;
+
+            _vue.sPosition = _vue.iPosition.ToString();
+
+            bResultat = _vue.DicImgFigure.TryGetValue("fig" + _vue.iPosition, out imgValue);
+            if (bResultat == true)
+                _vue.PositionAFaire = imgValue;
+
+        }
+
+        /// <summary>
+        /// Lorsqu'on appuie sur le bouton suivant ou précédent, modifier la figure en conséquence.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClickChangerFigure_Click(object sender, EventArgs e)
+        {
+            Control bouton = (Control)sender;
+
+            if (bouton.Name == "btnSuivant")
+                _vue.iPosition++;
+            else if (bouton.Name == "btnPrecedent")
+                _vue.iPosition--;
+
+            ChargerFigure();
+        }
+
+        private void btnApprendre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ JeuHoy/Vue/IVue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuHoy.Vue
+{
+    public interface IVue
+    {
+        string Console { get; set; }
+        string sPosition { get; set; }
+        int iPosition { get; set; }
+
+        Dictionary<string, Image> DicImgFigure { get; set; }
+        Image PositionAFaire { get; set; }
+        event EventHandler ChangerFigure;
+        event EventHandler Apprendre;
+
+    }
+}
